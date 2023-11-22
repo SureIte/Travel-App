82061b9d288e4444770adaa48f0cd234269c7cbb
SureITE <tithsure912@gmail.com>
2023-11-23 01:10:33 +0700

    Change new ActivityHome and add new file

--- app/src/main/java/kh/edu/rupp/ite/mytravel/view/fragment/HomeFragment.kt
+++ app/src/main/java/kh/edu/rupp/ite/mytravel/view/fragment/HomeFragment.kt
@@ -1,50 +1,38 @@
 package kh.edu.rupp.ite.mytravel.view.fragment
 
 import android.annotation.SuppressLint
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kh.edu.rupp.ite.mytravel.R
+import kh.edu.rupp.ite.mytravel.view.activity.HomeActivity
 
 class HomeFragment : Fragment() {
 
-    private var listener: OnButtonClickListener? = null
-
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
+            inflater: LayoutInflater,
+            container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.activity_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        // Button click listener
+        // Assuming there's a button with ID 'startHomeActivityButton'
         view.findViewById<View>(R.id.buttonInFragment).setOnClickListener {
-            listener?.onButtonClicked()
+            startHomeActivity()
         }
 
         return view
     }
 
-    interface OnButtonClickListener {
-        fun onButtonClicked()
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnButtonClickListener) {
-            listener = context
-        } else {
-            throw RuntimeException("$context must implement OnButtonClickListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
+    private fun startHomeActivity() {
+        val intent = Intent(activity, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
+
 
 
